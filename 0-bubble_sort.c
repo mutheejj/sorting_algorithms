68c216038466d02835232c4aa2a7ea77830f8455
Kymemia <kimemiachristopherr@gmail.com>
2024-02-21 00:57:49 +0300

    T1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,7 +13,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j +1] && array[j +1])
+			if (array[j] > array[j + 1] && array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
