9ba8526e370b88e09df4687d77e49b3578525362
mutheejj <johnmuthee547@gmail.com>
2024-02-21 05:30:52 -0800

    fixed the inner loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,7 +14,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
