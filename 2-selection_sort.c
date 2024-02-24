587e51c15e44190bda301be78f4c0cd94d6db868
mutheejj <johnmuthee547@gmail.com>
2024-02-24 13:11:55 -0800

    l

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,78 +1,33 @@
 #include "sort.h"
-
 /**
-  * quick_sort - ...
-  * @array: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-void quick_sort(int *array, size_t size)
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @size: size of the array
+ * @array: list with numbers
+ */
+void selection_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
+	size_t i, index;
+	int tmp, swap, flag = 0;
+
+	if (array == NULL)
 		return;
-
-	quick_sort_rec(array, 0, size - 1, size);
-}
-
-/**
-  * quick_sort_rec - ...
-  * @array: ...
-  * @lower: ...
-  * @higher: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-void quick_sort_rec(int *array, int lower, int higher, size_t size)
-{
-	int l_p = 0;
-
-	if (lower < higher)
+	for (i = 0; i < size; i++)
 	{
-		l_p = lomuto_partition(array, lower, higher, size);
-		quick_sort_rec(array, lower, l_p - 1, size);
-		quick_sort_rec(array, l_p + 1, higher, size);
-	}
-}
-
-/**
-  * lomuto_partition - ...
-  * @array: ...
-  * @lower: ...
-  * @higher: ...
-  * @size: ...
-  *
-  * Return: Nothing!
-  */
-int lomuto_partition(int *array, int lower, int higher, size_t size)
-{
-	int i = 0, j = 0, pivot = 0, aux = 0;
-
-	pivot = array[higher];
-	i = lower;
-
-	for (j = lower; j < higher; ++j)
-	{
-		if (array[j] < pivot)
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
 		{
-			aux = array[i];
-			array[i] = array[j];
-			array[j] = aux;
-
-			if (aux != array[i])
-				print_array(array, size);
-
-			++i;
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
 		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
 	}
-
-	aux = array[i];
-	array[i] = array[higher];
-	array[higher] = aux;
-
-	if (aux != array[i])
-		print_array(array, size);
-
-	return (i);
 }
